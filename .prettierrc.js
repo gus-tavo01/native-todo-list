d3869eb7486acae9097e8e4a42b19a1dfed896ac
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-11-05 08:31:28 -0700

    add base files

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 100,
+  useTabs: false,
+};
