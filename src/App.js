6ec8b7af7afc97e7b1caf5bac32fee88455da51c
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-11-09 17:05:19 -0800

    hide splash screen on app mount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 
 import Home from './views/Home';
 import Tasks from './views/Tasks';
@@ -11,6 +12,12 @@ import Tasks from './views/Tasks';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1500);
+  }, []);
+
   return (
     <Provider store={store}>
       <NativeBaseProvider>
