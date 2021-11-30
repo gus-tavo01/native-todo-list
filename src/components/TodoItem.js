70e58a392e1c8ef2cb2924519c593e4eb8fd96c7
gus-tavo01 <gustavoa.loera01@gmail.com>
2021-11-30 12:38:15 -0800

    add pressable behavior and colors

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,9 +11,23 @@ import {
   Container,
 } from 'native-base';
 
+const fontColor = '#000';
+const bgColor = 'blue.300';
+
 const TodoItem = (props) => {
-  const item = { id: props.id, name: props.name, isDone: props.isDone };
-  const { onMarkAsDone, onEdit, onDelete } = props;
+  const item = {
+    id: props.id,
+    name: props.name,
+    isDone: props.isDone,
+    description: props.description,
+  };
+  const { onMarkAsDone, onEdit, onDelete, onPress } = props;
+
+  const handleOnPress = () => {
+    if (item.description) {
+      onPress(item);
+    }
+  };
   return (
     <Stack
       direction="row"
@@ -24,22 +38,24 @@ const TodoItem = (props) => {
       px={2}
       my={3}
       rounded={15}
-      backgroundColor="#D3D3D3">
+      backgroundColor={bgColor}>
       <Container flexDirection="row" alignItems="center">
         <Checkbox
           value={item.isDone.toString()}
           my={3}
           mr={3}
           size="lg"
-          colorScheme="light"
+          colorScheme="info"
           rounded={20}
           isChecked={item.isDone}
           onChange={(selected) => onMarkAsDone(item, selected)}
           accessibilityLabel="is the task done"
         />
-        <Text mx={3} strikeThrough={item.isDone} fontSize={22}>
-          {item.name}
-        </Text>
+        <Pressable onPress={handleOnPress} accessibilitiLabel="Todo details">
+          <Text mx={3} strikeThrough={item.isDone} fontSize={22} color={fontColor}>
+            {item.name}
+          </Text>
+        </Pressable>
       </Container>
       <Menu
         w={110}
@@ -64,14 +80,17 @@ TodoItem.propTypes = {
   onEdit: PropTypes.func,
   onDelete: PropTypes.func,
   onMarkAsDone: PropTypes.func,
+  onPress: PropTypes.func,
 };
 
 TodoItem.defaultProps = {
   name: 'dummy',
   isDone: false,
+  description: null,
   onEdit: () => null,
   onDelete: () => null,
   onMarkAsDone: () => null,
+  onPress: () => null,
 };
 
 export default TodoItem;
